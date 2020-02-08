82b831021e29f39c84eb0573f3b31b0f9df73243
Ilya Petrov <dnamix1@gmail.com>
2020-02-08 14:07:53 +0300

    search messages with RegExp and reduce JS method

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -3,7 +3,7 @@ import { Header, Segment, Input, Icon } from "semantic-ui-react";
 
 class MessagesHeader extends React.Component {
   render() {
-    const { channelName, numUniqueUsers } = this.props;
+    const { channelName, numUniqueUsers, handleSearchChange } = this.props;
     return (
       <Segment clearing>
         {/* Channel title */}
@@ -21,6 +21,7 @@ class MessagesHeader extends React.Component {
         </Header>
         <Header floated="right">
           <Input
+            onChange={handleSearchChange}
             size="mini"
             icon="search"
             name="searchTerm"
