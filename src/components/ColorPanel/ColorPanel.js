cc520fe1b6640165fea31c3ac564f7464e93f193
Ilya Petrov <dnamix1@gmail.com>
2020-02-13 11:08:57 +0300

    color picking modal development started

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,7 +1,29 @@
 import React from "react";
-import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
+import {
+  Sidebar,
+  Menu,
+  Divider,
+  Button,
+  Modal,
+  Icon,
+  Label,
+  Segment
+} from "semantic-ui-react";
+import { SketchPicker } from "react-color";
 class ColorPanel extends React.Component {
+  state = {
+    modal: false,
+    primary: "",
+    secondary: ""
+  };
+
+  openModal = () => this.setState({ modal: true });
+  closeModal = () => this.setState({ modal: false });
+
+  handleChangePrimary = color => this.setState({ primary: color.hex });
+  handleChangeSecondary = color => this.setState({ secondary: color.hex });
   render() {
+    const { modal, primary, secondary } = this.state;
     return (
       <Sidebar
         as={Menu}
@@ -12,7 +34,37 @@ class ColorPanel extends React.Component {
         width="very thin"
       >
         <Divider />
-        <Button icon="add" size="small" color="red" />
+        <Button icon="add" size="small" color="red" onClick={this.openModal} />
+        {/* Color Picker Modal */}
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Choose App Colors</Modal.Header>
+          <Modal.Content>
+            <Segment className="color-picker" inverted>
+              <Label content="Primary color" />
+              <SketchPicker
+                onChange={this.handleChangePrimary}
+                color={primary}
+              />
+            </Segment>
+            <Segment className="color-picker" inverted>
+              <Label content="Secondary color" />
+              <SketchPicker
+                onChange={this.handleChangeSecondary}
+                color={secondary}
+              />
+            </Segment>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" />
+              Save Colors
+            </Button>
+            <Button color="red" inverted onClick={this.closeModal}>
+              <Icon name="remove" />
+              Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </Sidebar>
     );
   }
