88049dff6e07bd9ba08380e0739de71151d2106b
Ilya Petrov <dnamix1@gmail.com>
2020-02-19 14:42:43 +0300

    setup for user to be able to change avatar - modal, close-open functions and based on FileReader Web API ability to preview and send the image to database

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,10 +1,33 @@
 import React from "react";
 import firebase from "../../firebase.utils";
-import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
+import {
+  Grid,
+  Header,
+  Icon,
+  Dropdown,
+  Image,
+  Modal,
+  Input,
+  Button
+} from "semantic-ui-react";
 
 class UserPanel extends React.Component {
   state = {
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    modal: false
+  };
+  openModal = () => this.setState({ modal: true });
+  closeModal = () => this.setState({ modal: false });
+  handleChange = event => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.addEventListener("load", () => {
+        this.setState({});
+      });
+    }
   };
   dropdownOptions = () => [
     {
@@ -18,7 +41,7 @@ class UserPanel extends React.Component {
     },
     {
       key: "avatar",
-      text: <span>Change Avatar</span>
+      text: <span onClick={this.openModal}>Change Avatar</span>
     },
     {
       key: "signout",
@@ -34,7 +57,7 @@ class UserPanel extends React.Component {
   };
 
   render() {
-    const { user } = this.state;
+    const { user, modal } = this.state;
     const { primaryColor } = this.state;
     return (
       <Grid style={{ background: primaryColor }}>
@@ -57,6 +80,41 @@ class UserPanel extends React.Component {
               />
             </Header>
           </Grid.Row>
+          {/* Change user avatar modal */}
+          <Modal basic open={modal} onClose={this.closeModal}>
+            <Modal.Header>Change Avatar</Modal.Header>
+            <Modal.Content>
+              <Input
+                onChange={this.handleChange}
+                fluid
+                type="file"
+                label="New Avatar"
+                name="previewImage"
+              />
+              <Grid centered stackable columns={2}>
+                <Grid.Row centered>
+                  <Grid.Column className="ui center aligned grid">
+                    {/* Image Preview */}
+                  </Grid.Column>
+                  <Grid.Column>{/* Cropped Image Preview */}</Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color="green" inverted>
+                <Icon name="save" />
+                Change Avatar
+              </Button>
+              <Button color="green" inverted>
+                <Icon name="image" />
+                Preview
+              </Button>
+              <Button color="red" inverted onClick={this.closeModal}>
+                <Icon name="remove" />
+                Cancel
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </Grid.Column>
       </Grid>
     );
